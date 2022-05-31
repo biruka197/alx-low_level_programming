33860cd863d2e44eb805a9af12a6441430546988
biruk <birukkassahun555@gmail.com>
2022-05-31 14:39:26 -0700

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void){
- puts("\"Programming is like building a multilingual puzzle");
- return 0;
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
