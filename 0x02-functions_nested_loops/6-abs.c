2a0fd7fad120cca6fca85ec1f469a4c9b539f602
biruk <birukkassahun555@gmail.com>
2022-06-07 14:34:02 -0700

    function and nested loop

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,20 +7,23 @@
  *@n: The int to print
  * Return: Always 0.
  */
+
 int _abs(int n)
 {
-	if (n < 0)
-	{
-		return (n * (-1));
 
-	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	
-	else 
-	{
-	        return (n);
-	}
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
+
 }
