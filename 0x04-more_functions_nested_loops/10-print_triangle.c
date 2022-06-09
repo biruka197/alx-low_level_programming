f0aec265492c59b1214da9082c89f5f0e908b933
biruk <birukkassahun555@gmail.com>
2022-06-09 11:13:06 -0700

    function and nested loop

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * print_trianle -check for digit
+ * return:void
+ * 
+ */
+
+void print_triangle(int size)
+{
+
+	int i = 1, i1;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (i2 < size - i)
+		{
+			_putchar(' ');
+			i1++;
+		}
+		ii = 0;
+		while (i1 < i)
+		{
+			_putchar('#');
+			i1++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
