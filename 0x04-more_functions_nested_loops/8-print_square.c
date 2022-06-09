6d6beb1f5574beb61d0922bb14cb39879fc2d93b
biruk <birukkassahun555@gmail.com>
2022-06-09 10:56:55 -0700

    nested loop and function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_square(int n)
+{
+
+	int i = 0, i1;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (i1 < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+
+}
