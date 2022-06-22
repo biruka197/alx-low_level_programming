d68aa13ecac18a06b1c7fbfa52f3ce44de2f504c
biruk <birukkassahun555@gmail.com>
2022-06-22 10:43:13 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts_recursion - main - check the code
  * @s: character of string
@@ -9,9 +9,9 @@ void _puts_recursion(char *s)
 {
 	if (*(s) != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		putchar(*s);
+		puts_recursion(s + 1);
 	}
 	if (*(s) == '\0')
-		_putchar('\n');
+		putchar('\n');
 }
