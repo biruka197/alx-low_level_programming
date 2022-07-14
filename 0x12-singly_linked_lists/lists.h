6abb24443d637bb19224f24a92fd1eddb7cde199
biruk <birukkassahun555@gmail.com>
2022-07-14 14:52:45 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
+
 
 
 
