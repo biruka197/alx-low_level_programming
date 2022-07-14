b2bcf90db5f111823ed4e82319cfa8a4ff205f3f
biruk <birukkassahun555@gmail.com>
2022-07-14 14:58:24 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
 typedef struct list_s
 {
     char *str;
@@ -13,6 +23,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);i
 
 
 
