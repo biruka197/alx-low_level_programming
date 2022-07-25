83f7742fd59672c36995bb7810b30eead72220c4
biruk <birukkassahun555@gmail.com>
2022-07-25 14:51:09 -0700

    this is commit for 0x15-file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif 
